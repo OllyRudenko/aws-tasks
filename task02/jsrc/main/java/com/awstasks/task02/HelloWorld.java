38156048269181b8d3c9bfd6487f4d79ef9b1b39
olha_rudenko1 <olha_rudenko1@epam.com>
2024-01-10 15:34:12 +0200

    task02

---
+++ task02/jsrc/main/java/com/awstasks/task02/HelloWorld.java
@@ -0,0 +1,43 @@
+package com.awstasks.task02;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.Gson;
+import com.syndicate.deployment.annotations.LambdaUrlConfig;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@LambdaHandler(lambdaName = "hello_world",
+        roleName = "hello_world-role",
+        isPublishVersion = true
+)
+@LambdaUrlConfig(
+        authType = AuthType.NONE,
+        invokeMode = InvokeMode.BUFFERED
+)
+public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    private static final int SC_OK = 200;
+    private static final int SC_BAD_REQUEST = 400;
+    private final Gson gson = new Gson();
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
+        context.getLogger().log(apiGatewayProxyRequestEvent.toString());
+        Map<String, Object> body = new HashMap<>();
+        body.put("message", "Hello from Lambda");
+        try {
+            return new APIGatewayProxyResponseEvent()
+                    .withStatusCode(SC_OK)
+                    .withBody(gson.toJson(body));
+        } catch (IllegalArgumentException exception) {
+            return new APIGatewayProxyResponseEvent()
+                    .withStatusCode(SC_BAD_REQUEST);
+        }
+    }
+}

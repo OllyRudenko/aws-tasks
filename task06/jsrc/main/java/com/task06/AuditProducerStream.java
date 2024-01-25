c9ec846fc3b6ff9baa8d8bdd94dba297c4a4a6c4
olha_rudenko1 <olha_rudenko1@epam.com>
2024-01-26 00:05:36 +0200

    task06

--- task06/jsrc/main/java/com/task06/AuditProducerStream.java
+++ task06/jsrc/main/java/com/task06/AuditProducerStream.java
@@ -1,4 +1,4 @@
-package com.stream.dynamobd.task06;
+package com.task06;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;

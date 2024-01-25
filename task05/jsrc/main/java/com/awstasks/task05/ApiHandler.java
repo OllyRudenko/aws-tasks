da92a8ad08046995c298a812c1a7731adcbacba1
olha_rudenko1 <olha_rudenko1@epam.com>
2024-01-25 09:47:09 +0200

    task05

---
+++ task05/jsrc/main/java/com/awstasks/task05/ApiHandler.java
@@ -0,0 +1,124 @@
+package com.awstasks.task05;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.GetItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PrimaryKey;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.UpdateItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
+import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import com.amazonaws.services.dynamodbv2.model.PutItemResult;
+import com.amazonaws.services.dynamodbv2.model.ReturnValue;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.services.s3.AmazonS3;
+import com.awstasks.task05.model.Event;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
+import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@LambdaHandler(lambdaName = "api_handler",
+        roleName = "api_handler-role"
+)
+@EnvironmentVariables(value = {
+        @EnvironmentVariable(key = "region", value = "${region}"),
+        @EnvironmentVariable(key = "target_table", value = "${target_table}")
+})
+public class ApiHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    private static final String TABLE_NAME = "Events";
+    private static final String INSERT = "INSERT";
+    private AmazonS3 s3Client;
+    private ObjectMapper objectMapper;
+    JSONParser jsonParser = new JSONParser();
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
+        String requestBody = request.getBody();
+        System.out.println("!!!!!!! Hello " + requestBody);
+        AmazonDynamoDB clientDynamoDB = AmazonDynamoDBClientBuilder.standard()
+                .withRegion(System.getenv("region")).build();
+        DynamoDB dynamoDB = new DynamoDB(clientDynamoDB);
+        Event event = getEventFromRequest(requestBody);
+
+        // uuid
+        String eventID = generateUniqueID();
+
+        // save to DB
+        Table table = dynamoDB.getTable(System.getenv("target_table"));
+
+        PutItemOutcome outcome = table
+                .putItem(new PutItemSpec().withItem(new Item().withString("id", eventID)
+                        .withNumber("principalId", event.getPrincipalId())
+                        .withString("createdAt", event.getCreatedAt().toString())
+                        .withMap("body", event.getBody())));
+
+        // get from DB saved object
+        GetItemOutcome getItemOutcome = table.getItemOutcome(
+                new PrimaryKey("principalId", event.getPrincipalId()));
+        Item eventNew = getItemOutcome.getItem();
+
+
+        System.out.println("eventNew " + eventNew.toJSON());
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        response.setStatusCode(201);
+        response.setBody(eventNew.toJSON());
+        return response;
+    }
+
+    private Event getEventFromRequest(String requestBody) {
+        Event event = new Event();
+        JSONObject reqObject;
+
+        try {
+            reqObject = (JSONObject) jsonParser.parse(requestBody);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        event.setId(generateUniqueID());
+        event.setPrincipalId(Integer.valueOf((reqObject.get("principalId").toString())));
+        event.setCreatedAt(LocalDateTime.now());
+        JSONObject bodyJson;
+        try {
+            bodyJson = (JSONObject) new JSONParser().parse(reqObject.get("content").toString());
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        Map<String, String> body = getBodyValues(bodyJson);
+        event.setBody(body);
+
+        return event;
+    }
+
+    private Map<String, String> getBodyValues(JSONObject bodyJson) {
+        Map<String, String> body = new LinkedHashMap<>();
+        body.put("name", bodyJson.get("name").toString());
+        body.put("surname", bodyJson.get("surname").toString());
+        return body;
+    }
+
+    private String generateUniqueID() {
+        return UUID.randomUUID().toString();
+    }
+
+}
---
+++ task05/jsrc/main/java/com/awstasks/task05/model/Event.java
@@ -0,0 +1,79 @@
+package com.awstasks.task05.model;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+
+public class Event {
+    private String id;
+    private Integer principalId;
+    private LocalDateTime createdAt;
+    private Map<String, String> body;
+
+    public Event() {
+    }
+
+    public Event(String id, Integer principalId, LocalDateTime createdAt, Map<String, String> body) {
+        this.id = id;
+        this.principalId = principalId;
+        this.createdAt = createdAt;
+        this.body = body;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getPrincipalId() {
+        return principalId;
+    }
+
+    public void setPrincipalId(Integer principalId) {
+        this.principalId = principalId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Map<String, String> getBody() {
+        return body;
+    }
+
+    public void setBody(Map<String, String> body) {
+        this.body = body;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Event)) return false;
+        Event event = (Event) o;
+        return getId().equals(event.getId()) && getPrincipalId().equals(event.getPrincipalId())
+                && getCreatedAt().equals(event.getCreatedAt()) && getBody().equals(event.getBody());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getPrincipalId(), getCreatedAt(), getBody());
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "id=" + id +
+                ", principalId=" + principalId +
+                ", createdAt=" + createdAt +
+                ", body=" + body +
+                '}';
+    }
+}

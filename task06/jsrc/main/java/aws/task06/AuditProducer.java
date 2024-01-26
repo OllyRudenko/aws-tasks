d01654c1210034657544842f13a48591795f5c7c
olha_rudenko1 <olha_rudenko1@epam.com>
2024-01-26 11:32:29 +0200

    task06

--- task06/jsrc/main/java/aws/task06/AuditProducer.java
+++ task06/jsrc/main/java/aws/task06/AuditProducer.java
@@ -24,6 +24,7 @@ import java.util.UUID;
 
 @LambdaHandler(lambdaName = "audit_producer",
         roleName = "audit_producer-role",
+        isPublishVersion = true,
         aliasName = "${lambdas_alias_name}"
 )
 @DynamoDbTriggerEventSource(targetTable = "Configuration", batchSize = 1)
